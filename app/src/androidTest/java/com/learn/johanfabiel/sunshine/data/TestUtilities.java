eef2efc217486b2d5ab2dbae1198f74f21ad6d6d
fabiel casas <fabiel@rokk3rlabs.com>
2015-03-31 10:56:35 -0500

    add contrarts provider

--- app/src/androidTest/java/com/learn/johanfabiel/sunshine/data/TestUtilities.java
+++ app/src/androidTest/java/com/learn/johanfabiel/sunshine/data/TestUtilities.java
@@ -8,7 +8,8 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.test.AndroidTestCase;
 
-import com.example.android.sunshine.app.utils.PollingCheck;
+
+import com.learn.johanfabiel.sunshine.utils.PollingCheck;
 
 import java.util.Map;
 import java.util.Set;
